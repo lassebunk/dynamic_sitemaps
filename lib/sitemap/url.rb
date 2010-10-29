230349c4ca8b6a33eacc9b775ae2cbcf51df1551
Lasse Bunk <lassebunk@gmail.com>
2010-10-29 22:42:47 +0200

    remove unused code

--- lib/sitemap/url.rb
+++ lib/sitemap/url.rb
@@ -5,14 +5,5 @@ module Sitemap
     def initialize(loc, options = {})
       @loc, @last_mod, @change_freq, @priority = loc, options[:last_mod], options[:change_freq], options[:priority]
     end
-
-    def convert_option(option, obj = nil)
-      if option.is_a?(Symbol) && !obj.nil?
-        option = obj.send(option)
-      elsif option.is_a?(Proc)
-        option = option.call(obj)
-      end
-      option
-    end
   end
 end
