5aec04e64df2260ab2098084a2cfaa098fcd6545
Lasse Bunk <lassebunk@gmail.com>
2011-09-18 00:08:04 +0200

    change generator to support initializer name instead of route name

--- lib/generators/dynamic_sitemaps/dynamic_sitemaps_generator.rb
+++ lib/generators/dynamic_sitemaps/dynamic_sitemaps_generator.rb
@@ -2,10 +2,10 @@ class DynamicSitemapsGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   
   def create_initializer
-    copy_file "initializer.rb", "config/initializers/sitemap.rb"
+    copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
   end
 
   def create_route
-    route "match '#{file_name}' => 'sitemaps#sitemap'"
+    route "match 'sitemap.xml' => 'sitemaps#sitemap'"
   end
 end
