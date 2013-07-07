99d3eb4628b07a65c2049711c238af894a1f982f
Lasse Bunk <lassebunk@gmail.com>
2013-07-07 17:43:09 +0200

    Use model name instead of table name

--- lib/dynamic_sitemaps/generator.rb
+++ lib/dynamic_sitemaps/generator.rb
@@ -21,7 +21,7 @@ module DynamicSitemaps
       def sitemap_for(collection, options = {}, &block)
         raise "The collection given to `sitemap_for` must respond to #find_each. This is for performance. Use `Model.scoped` to get an ActiveRecord relation that responds to #find_each." unless collection.respond_to?(:find_each)
 
-        name = options.delete(:name) || collection.table_name.to_sym
+        name = options.delete(:name) || collection.model_name.underscore.pluralize.to_sym
         options[:collection] = collection
 
         sitemap(name, options, &block)
