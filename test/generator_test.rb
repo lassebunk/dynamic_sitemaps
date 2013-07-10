88d26bf272303c865e7279614e902b6b02e19eb9
Lasse Bunk <lassebunk@gmail.com>
2013-07-10 18:48:34 +0200

    Convert time to UTC

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -26,7 +26,7 @@ class GeneratorTest < ActiveSupport::TestCase
     url = doc.xpath("urlset/url")
     assert_equal 1, url.count
     assert_equal "http://www.mytest.com/", url.xpath("loc").text
-    assert_equal "2013-10-07T05:46:23+02:00", url.xpath("lastmod").text
+    assert_equal "2013-10-07T03:46:23+00:00", url.xpath("lastmod").text
     assert_equal "daily", url.xpath("changefreq").text
     assert_equal "1.0", url.xpath("priority").text
 
