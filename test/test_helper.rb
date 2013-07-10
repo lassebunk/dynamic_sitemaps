94d738af90aace05e4120cbf2f272e5fb314a842
Lasse Bunk <lassebunk@gmail.com>
2013-07-10 17:16:19 +0200

    Add Webmock

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+require "webmock/test_unit"
 
 Rails.backtrace_cleaner.remove_silencers!
 
