638ff4f95fa13c03f75033a315eddccdd06ead23
Lasse Bunk <lassebunk@gmail.com>
2013-07-10 17:39:19 +0200

    Raise error on blank paths

--- test/dynamic_sitemaps_test.rb
+++ test/dynamic_sitemaps_test.rb
@@ -13,5 +13,24 @@ class DynamicSitemapsTest < ActiveSupport::TestCase
     assert_equal Rails.root.join("config", "sitemap.rb").to_s, DynamicSitemaps.config_path
     assert_equal 50000, DynamicSitemaps.per_page
   end
+
+  test "raises error on blank paths" do
+    assert_nothing_raised do
+      DynamicSitemaps.path = "/my/test/folder"
+      DynamicSitemaps.folder = "my_sitemaps"
+      DynamicSitemaps.config_path = "/my/config.rb"
+    end
+
+    assert_raises ArgumentError do
+      DynamicSitemaps.path = ""
+    end
+
+    assert_raises ArgumentError do
+      DynamicSitemaps.folder = ""
+    end
+
+    assert_raises ArgumentError do
+      DynamicSitemaps.config_path = ""
+    end
   end
 end
