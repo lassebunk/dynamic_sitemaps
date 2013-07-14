4f5021cadbccfb7d0be801788c739380389f2c47
Lasse Bunk <lassebunk@gmail.com>
2013-07-14 18:40:30 +0200

    Add more examples to configuration template

--- lib/generators/dynamic_sitemaps/templates/config.rb
+++ lib/generators/dynamic_sitemaps/templates/config.rb
@@ -1,10 +1,26 @@
+# Change this to your host. See the readme at https://github.com/lassebunk/dynamic_sitemaps
+# for examples of multiple hosts and folders.
 host "www.example.com"
 
 sitemap :site do
   url root_url, last_mod: Time.now, change_freq: "daily", priority: 1.0
-
-  # Ping search engines after sitemap generation
-  # ping_with "http://#{host}/sitemap.xml"
-  
-  # TODO: Add examples
 end
+
+# You can have multiple sitemaps like the above – just make sure their names are different.
+
+# Automatically link to all pages using the routes specified
+# using "resources :pages" in config/routes.rb. This will also
+# automatically set <lastmod> to the date and time in page.updated_at:
+#
+# sitemap_for Page.scoped
+
+# For products with special sitemap name and priority, and link to comments:
+#
+# sitemap_for Product.published, name: :published_products do |product|
+#   url product, last_mod: product.updated_at, priority: (product.featured? ? 1.0 : 0.7)
+#   url product_comments_url(product)
+# end
+
+# Ping search engines after sitemap generation:
+#
+# ping_with "http://#{host}/sitemap.xml"
