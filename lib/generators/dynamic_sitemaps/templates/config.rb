72ec54bfed9d26c2c6c13f9e0284dcd35cb3c659
Lasse Bunk <lassebunk@gmail.com>
2013-08-03 14:24:07 +0200

    Add folder example to generated sitemap config file

--- lib/generators/dynamic_sitemaps/templates/config.rb
+++ lib/generators/dynamic_sitemaps/templates/config.rb
@@ -12,15 +12,29 @@ end
 # using "resources :pages" in config/routes.rb. This will also
 # automatically set <lastmod> to the date and time in page.updated_at:
 #
-# sitemap_for Page.scoped
+#   sitemap_for Page.scoped
 
 # For products with special sitemap name and priority, and link to comments:
 #
-# sitemap_for Product.published, name: :published_products do |product|
-#   url product, last_mod: product.updated_at, priority: (product.featured? ? 1.0 : 0.7)
-#   url product_comments_url(product)
-# end
+#   sitemap_for Product.published, name: :published_products do |product|
+#     url product, last_mod: product.updated_at, priority: (product.featured? ? 1.0 : 0.7)
+#     url product_comments_url(product)
+#   end
+
+# If you want to generate multiple sitemaps in different folders (for example if you have
+# more than one domain, you can specify a folder before the sitemap definitions:
+# 
+#   Site.all.each do |site|
+#     folder "sitemaps/#{site.domain}"
+#     host site.domain
+#     
+#     sitemap :site do
+#       url root_url
+#     end
+# 
+#     sitemap_for site.products.scoped
+#   end
 
 # Ping search engines after sitemap generation:
 #
-# ping_with "http://#{host}/sitemap.xml"
+#   ping_with "http://#{host}/sitemap.xml"
