508a51302b10c6c7b35a606c1bf949e9a4e023bc
Dalton <dalthon@dlt.local>
2014-02-25 15:33:01 -0300

    Forgot that

--- app/controllers/dynamic_sitemaps_controller.rb
+++ app/controllers/dynamic_sitemaps_controller.rb
@@ -1,6 +1,6 @@
 class DynamicSitemapsController < ApplicationController
   def sitemap
-    sitemap = Sitemap.where(path: request.path).first
+    sitemap = ::Sitemap.where(path: request.path).first
 
     if sitemap
       render text: sitemap.content
