77bcb11b5b9791227f7c89be8a44242fdc5d2eab
Dalton <dalthon@dlt.local>
2014-02-25 14:46:20 -0300

    Adds Sitemap model to Engine

--- lib/dynamic_sitemaps/storage/database_storage.rb
+++ lib/dynamic_sitemaps/storage/database_storage.rb
@@ -2,7 +2,7 @@ module DynamicSitemaps
   class DatabaseStorage < Storage
 
     def move_to_destination
-      Sitemap.delete_all
+      ::Sitemap.delete_all
       generator.sitemaps.map(&:folder).uniq.each do |folder|
         temp_files = File.join(DynamicSitemaps.temp_path, folder, "*.xml")
         Dir.glob(temp_files).each do |temp_file|
@@ -14,7 +14,7 @@ module DynamicSitemaps
 
     protected
     def save_to_database(temp_file, destination)
-      Sitemap.create! path: destination, content: File.read(temp_file)
+      ::Sitemap.create! path: destination, content: File.read(temp_file)
     end
 
   end
